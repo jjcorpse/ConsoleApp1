2d75f4207709f770a1aa286599a08bbccac7aa5d
ÄGARE <GARE@DESKTOP-9FCGT42>
2023-08-20 20:32:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.CodeDom.Compiler;
+
+Console.WriteLine("Hello, World it work!");
+Console .clear temp "run/settings/clean/cashe" (C:/TempFileCollection) "delete/temp/file 1"
+ console.""xnor=1" end program 1
